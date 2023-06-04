d019db0e753366142f700b6ac886f0f1fd6d7c9b
Joe-Etubi Great Attai <greatattai442442@gmail.com>
2023-06-04 04:22:38 -0800

    improvements

--- JumiaReviews/content.js
+++ JumiaReviews/content.js
@@ -17,5 +17,26 @@ function scrapeReviews() {
     return reviews;
 }
 
+function getProductInfo() {
+    var productImage = document.querySelector('div.sldr._img._prod.-rad4.-oh.-mbs a.itm img');
+    var productName = document.querySelector('div.-fs0.-pls.-prl h1.-fs20.-pts.-pbxs');
+
+    var productInfo = {
+        imageUrl: productImage ? productImage.src : '',
+        productName: productName ? productName.textContent.trim() : ''
+    };
+
+    return productInfo;
+}
+
 var scrapedReviews = scrapeReviews();
-chrome.runtime.sendMessage({ action: 'displayReviews', reviews: scrapedReviews });
+var productInfo = getProductInfo();
+
+if (productInfo.productName === '') {
+    chrome.runtime.sendMessage({ action: 'displayErrorMessage', message: 'Please provide a link to a Jumia product page.' });
+} else if (scrapedReviews.length === 0) {
+    chrome.runtime.sendMessage({ action: 'displayErrorMessage', message: 'No reviews found for this product.' });
+} else {
+    chrome.runtime.sendMessage({ action: 'displayReviews', reviews: scrapedReviews });
+    chrome.runtime.sendMessage({ action: 'displayProductInfo', productName: productInfo.productName, imageUrl: productInfo.imageUrl });
+}
--- JumiaReviews/popup.js
+++ JumiaReviews/popup.js
@@ -6,31 +6,59 @@ document.addEventListener('DOMContentLoaded', function() {
 function scrapeReviews() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var tab = tabs[0];
-        if (tab.url.includes('jumia.com.ng')) {
+        var url = tab.url;
+        if (url.includes('jumia.com.ng')) {
             chrome.tabs.executeScript(tab.id, { file: 'content.js' }, function() {
                 if (chrome.runtime.lastError) {
                     console.error(chrome.runtime.lastError);
                 }
             });
+        } else {
+            displayErrorMessage('Please go to a Jumia product page to fetch reviews.');
         }
     });
 }
 
+function displayReviews(reviews) {
+    var reviewsList = document.getElementById('reviewsList');
+    var existingReviewsCount = reviewsList.getElementsByTagName('li').length;
+
+    if (reviews.length > existingReviewsCount) {
+        for (var i = existingReviewsCount; i < reviews.length; i++) {
+            var reviewItem = document.createElement('li');
+            reviewItem.textContent = reviews[i].header + ': ' + reviews[i].text;
+            reviewsList.appendChild(reviewItem);
+        }
+        displaySuccessMessage('New reviews have been added.');
+    } else {
+        displaySuccessMessage('No new reviews found.');
+    }
+}
+
+function displayProductInfo(productName, imageUrl) {
+    var productNameElement = document.getElementById('productName');
+    var productImageElement = document.getElementById('productImage');
+    productNameElement.textContent = productName;
+    productImageElement.src = imageUrl;
+}
+
+function displayErrorMessage(message) {
+    var errorMessage = document.getElementById('errorMessage');
+    errorMessage.textContent = message;
+}
+
+function displaySuccessMessage(message) {
+    var errorMessage = document.getElementById('errorMessage');
+    errorMessage.style.color = '#008000';
+    errorMessage.textContent = message;
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'displayReviews') {
-        var reviewsList = document.getElementById('reviewsList');
-        var reviews = request.reviews;
-
-        if (reviews.length > 0) {
-            for (var i = 0; i < reviews.length; i++) {
-                var reviewItem = document.createElement('li');
-                reviewItem.textContent = reviews[i].header + ': ' + reviews[i].text;
-                reviewsList.appendChild(reviewItem);
-            }
-        } else {
-            var noReviewsItem = document.createElement('li');
-            noReviewsItem.textContent = 'No reviews found.';
-            reviewsList.appendChild(noReviewsItem);
-        }
+        displayReviews(request.reviews);
+    } else if (request.action === 'displayProductInfo') {
+        displayProductInfo(request.productName, request.imageUrl);
+    } else if (request.action === 'displayErrorMessage') {
+        displayErrorMessage(request.message);
     }
 });
